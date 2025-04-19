958834177fb5ea63aef7a07a43a03beab362cb37
UsamaKhushnood <usamakhushnood4@gmail.com>
2025-04-19 08:32:03 +0500

    change

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,18 +1,7 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from '@/components/ui/tabs'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import DateRangePicker from '@/components/dashboard/DateRangePicker.vue'
 import MainNav from '@/components/dashboard/MainNav.vue'
 import Overview from '@/components/dashboard/Overview.vue'
@@ -24,17 +13,6 @@ import UserNav from '@/components/dashboard/UserNav.vue'
 </script>
 
 <template>
-  <div class="md:hidden">
-    <VPImage
-      alt="Dashboard"
-      width="1280"
-      height="1214" class="block" :image="{
-        dark: '/examples/dashboard-dark.png',
-        light: '/examples/dashboard-light.png',
-      }"
-    />
-  </div>
-
   <div class="hidden flex-col md:flex">
     <div class="border-b">
       <div class="flex h-16 items-center px-4">
@@ -48,9 +26,7 @@ import UserNav from '@/components/dashboard/UserNav.vue'
     </div>
     <div class="flex-1 space-y-4 p-8 pt-6">
       <div class="flex items-center justify-between space-y-2">
-        <h2 class="text-3xl font-bold tracking-tight">
-          Dashboard
-        </h2>
+        <h2 class="text-3xl font-bold tracking-tight">Dashboard</h2>
         <div class="flex items-center space-x-2">
           <DateRangePicker />
           <Button>Download</Button>
@@ -58,26 +34,16 @@ import UserNav from '@/components/dashboard/UserNav.vue'
       </div>
       <Tabs default-value="overview" class="space-y-4">
         <TabsList>
-          <TabsTrigger value="overview">
-            Overview
-          </TabsTrigger>
-          <TabsTrigger value="analytics" disabled>
-            Analytics
-          </TabsTrigger>
-          <TabsTrigger value="reports" disabled>
-            Reports
-          </TabsTrigger>
-          <TabsTrigger value="notifications" disabled>
-            Notifications
-          </TabsTrigger>
+          <TabsTrigger value="overview"> Overview</TabsTrigger>
+          <TabsTrigger value="analytics" disabled> Analytics</TabsTrigger>
+          <TabsTrigger value="reports" disabled> Reports</TabsTrigger>
+          <TabsTrigger value="notifications" disabled> Notifications</TabsTrigger>
         </TabsList>
         <TabsContent value="overview" class="space-y-4">
           <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
             <Card>
               <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle class="text-sm font-medium">
-                  Total Revenue
-                </CardTitle>
+                <CardTitle class="text-sm font-medium"> Total Revenue</CardTitle>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -92,19 +58,13 @@ import UserNav from '@/components/dashboard/UserNav.vue'
                 </svg>
               </CardHeader>
               <CardContent>
-                <div class="text-2xl font-bold">
-                  $45,231.89
-                </div>
-                <p class="text-xs text-muted-foreground">
-                  +20.1% from last month
-                </p>
+                <div class="text-2xl font-bold">$45,231.89</div>
+                <p class="text-xs text-muted-foreground">+20.1% from last month</p>
               </CardContent>
             </Card>
             <Card>
               <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle class="text-sm font-medium">
-                  Subscriptions
-                </CardTitle>
+                <CardTitle class="text-sm font-medium"> Subscriptions</CardTitle>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -121,19 +81,13 @@ import UserNav from '@/components/dashboard/UserNav.vue'
                 </svg>
               </CardHeader>
               <CardContent>
-                <div class="text-2xl font-bold">
-                  +2350
-                </div>
-                <p class="text-xs text-muted-foreground">
-                  +180.1% from last month
-                </p>
+                <div class="text-2xl font-bold">+2350</div>
+                <p class="text-xs text-muted-foreground">+180.1% from last month</p>
               </CardContent>
             </Card>
             <Card>
               <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle class="text-sm font-medium">
-                  Sales
-                </CardTitle>
+                <CardTitle class="text-sm font-medium"> Sales</CardTitle>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -149,19 +103,13 @@ import UserNav from '@/components/dashboard/UserNav.vue'
                 </svg>
               </CardHeader>
               <CardContent>
-                <div class="text-2xl font-bold">
-                  +12,234
-                </div>
-                <p class="text-xs text-muted-foreground">
-                  +19% from last month
-                </p>
+                <div class="text-2xl font-bold">+12,234</div>
+                <p class="text-xs text-muted-foreground">+19% from last month</p>
               </CardContent>
             </Card>
             <Card>
               <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle class="text-sm font-medium">
-                  Active Now
-                </CardTitle>
+                <CardTitle class="text-sm font-medium"> Active Now</CardTitle>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -176,12 +124,8 @@ import UserNav from '@/components/dashboard/UserNav.vue'
                 </svg>
               </CardHeader>
               <CardContent>
-                <div class="text-2xl font-bold">
-                  +573
-                </div>
-                <p class="text-xs text-muted-foreground">
-                  +201 since last hour
-                </p>
+                <div class="text-2xl font-bold">+573</div>
+                <p class="text-xs text-muted-foreground">+201 since last hour</p>
               </CardContent>
             </Card>
           </div>
@@ -197,9 +141,7 @@ import UserNav from '@/components/dashboard/UserNav.vue'
             <Card class="col-span-3">
               <CardHeader>
                 <CardTitle>Recent Sales</CardTitle>
-                <CardDescription>
-                  You made 265 sales this month.
-                </CardDescription>
+                <CardDescription> You made 265 sales this month.</CardDescription>
               </CardHeader>
               <CardContent>
                 <RecentSales />
